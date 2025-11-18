8512edd888a16f3dc5e6023160892f4002e114ac
AngelBachler <angel.bachler@uc.cl>
2025-11-18 15:19:30 -0300

    h

--- src/vote_utils.py
+++ src/vote_utils.py
@@ -4,6 +4,8 @@ from .common_utils import safe_serialize
 from zeep import Client
 from tqdm.notebook import tqdm
 import logging
+import requests
+import xml.etree.ElementTree as ET
 
 def get_votaciones(client, year: int):
     res = client.service.retornarVotacionesXAnno(year)
@@ -51,4 +53,49 @@ def build_detalle_periodo(nombre_periodo: str):
             logging.error(f"Error al procesar año {year}: {e}")
     detalles_periodo_df = pd.concat(detalles_list, ignore_index=True)
     return detalles_periodo_df
+
+
+def procesar_votacion_detalle(xml_content):
+    # 1. Parsear el XML
+    try:
+        root = ET.fromstring(xml_content)
+    except ET.ParseError:
+        return pd.DataFrame()
+
+    # 2. Manejar el Namespace (CRÍTICO)
+    # El XML tiene xmlns="http://tempuri.org/", así que todas las etiquetas
+    # tienen este prefijo invisible. Lo definimos aquí.
+    ns = {'ns': 'http://tempuri.org/'}
+
+    # 3. Extraer Datos de Cabecera (Contexto de la votación)
+    # Usamos .find() con el prefijo del namespace 'ns:'
+    votacion_id = root.find('ns:ID', ns).text
+    fecha_hora = root.find('ns:Fecha', ns).text
+    boletin = root.find('ns:Boletin', ns).text
+    resultado = root.find('ns:Resultado', ns).text
+    tipo_votacion = root.find('ns:Tipo', ns).text
+    
+    # Extraer info de la Sesión (Anidada)
+    sesion = root.find('ns:Sesion', ns)
+    sesion_id = sesion.find('ns:ID', ns).text if sesion is not None else None
+    sesion_num = sesion.find('ns:Numero', ns).text if sesion is not None else None
+    sesion_tipo = sesion.find('ns:Tipo', ns).text if sesion is not None else None
+    tramite = root.find('ns:Tramite', ns).text if sesion is not None else None
+    informe = root.find('ns:Informe', ns).text if sesion is not None else None
+
+    info = {
+        # --- Contexto General ---
+        'votacion_id': votacion_id,
+        'fecha_hora': fecha_hora,
+        'boletin': boletin,
+        'sesion': sesion_num,
+        'sesion_id' : sesion_id,
+        'resultado_general': resultado,
+        'tipo_votacion': tipo_votacion,
+        'tipo_sesion' : sesion_tipo,
+        'tramite' : tramite,
+        'informe' : informe
+    }  
+
+    return info
     
